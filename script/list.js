f4af49407c771e55a67bdb016069ccc62dc458f1
ndang.royalty <royaltyhopsin@gmail.com>
2024-12-03 13:10:46 +0100

    working

--- script/list.js
+++ script/list.js
@@ -3,6 +3,7 @@ const selectList = document.getElementById('sub')
 const b = document.getElementById('list')
 
 selectList.addEventListener("click", () => {
+  let input
   const ulitem = document.createElement('li')
   ulitem.innerHTML = boxType.value
   list.append(ulitem)
